2e0716622dffcbc6b921ff1a654162c1f2c424d5
Marta Nelson <p3ngu1n33@msn.com>
2016-02-22 00:20:16 -0800

    google 404 page first commit

---
+++ js/index.js
@@ -0,0 +1,22 @@
+$(document).ready(function() {
+    $('.page').funnyText({
+        speed: 300, 
+        borderColor: '#fbbc05', 
+        activeColor: '#34a853',
+        color: '#4285f4',
+        fontSize: '7em',
+        direction: 'both'
+    });
+  
+ $(document).ready(function() {
+    $('.oops').funnyText({
+      speed: 300, 
+        borderColor: '#34a853', 
+        activeColor: '#ea4335',
+        color: '#4285f4',
+        fontSize: '2em',
+        direction: 'both'
+    });
+});
+  
+});
